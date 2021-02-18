b87ed23c864af8b8f5779b8e1480b76b6dc0dad0
Ellie <ellieredmayne@hotmail.com>
2021-02-18 16:04:16 +0000

    task1 done

--- src/components/BasicCounter/index.js
+++ src/components/BasicCounter/index.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
+import useCounter from "../../hooks/useCounter";
+import useDocumentTitle from "../../hooks/useDocumentTitle";
+
+
+// const [state, dispatch] = useReducer(reducer, initialState);
 
 function BasicCounter() {
-  const [count, setCount] = useState(0);
-  function increment() {
-    setCount((count) => count + 1);
-  }
-  function decrement() {
-    setCount((count) => Math.max(0, count - 1));
-  }
+ const [count, increment, decrement] = useCounter()
+  useDocumentTitle(count)
+  
   return (
     <div>
       <h4>Basic Counter</h4>
---
+++ src/hooks/useDocumentTitle.js
@@ -0,0 +1,9 @@
+import {useEffect} from "react";
+
+function useDocumentTitle(newTitle) {
+    useEffect(() => {
+        document.title = newTitle;
+    }, [newTitle]);
+}
+
+export default useDocumentTitle;
